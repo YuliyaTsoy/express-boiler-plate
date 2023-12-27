5bb2c2bab5575acc54cca53c4027c4809108b8c5
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2023-12-27 13:44:43 -0500

    adds 404 route

--- server.js
+++ server.js
@@ -11,14 +11,15 @@ const PORT = process.env.PORT || 3001;
 // MIDDILEWARES
 // stuff to get the req.body
 app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 // static assets
 app.use(express.static("public"));
 
 // ROUTES
 // html routes - deliver pages
 // GET / - the home page
-app.get("/",(req,res)=> res.sendFile(path.join(__dirname, "public/index.html")))
+app.get("/",(req,res)=> 
+res.sendFile(path.join(__dirname, "public/index.html")))
 
 // api routes - deliver data
 app.get("/api/todos", (req, res)=>res.json ([
@@ -28,7 +29,7 @@ app.get("/api/todos", (req, res)=>res.json ([
 ]))
 // 404 route
 app.get("*", (req, res)=> {
-    app.get("/",(req,res)=> res.sendFile(path.join(__dirname, "public/404.html")))
+    res.sendFile(path.join(__dirname, "public/404.html"));
 })
 // START SERVER
 app.listen(PORT, () => console.log (`Server live on port ${PORT}`));
