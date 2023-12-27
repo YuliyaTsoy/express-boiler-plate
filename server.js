146112f45e823b9ecd18f0bd580862a028d25b78
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2023-12-27 13:33:34 -0500

    adds 2 base routes

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const express = require ("express");
 // APP / PORT
 const app = express();
 const PORT = process.env.PORT || 3001;
-// MIDDILEWARE
+
+// MIDDILEWARES
 // stuff to get the req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
@@ -18,6 +19,7 @@ app.use(express.static("public"));
 // html routes - deliver pages
 // GET / - the home page
 app.get("/",(req,res)=> res.sendFile(path.join(__dirname, "public/index.html")))
+
 // api routes - deliver data
 app.get("/api/todos", (req, res)=>res.json ([
     {
